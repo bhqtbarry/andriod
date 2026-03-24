44d7f7037067f62266d3c5b2c15017f98e2b26ba
Liu Shui <shui.liu@michelin.com>
2026-03-24 15:26:29 +0700

    1

--- app/src/main/java/cn/syphotos/android/SyPhotosApplication.kt
+++ app/src/main/java/cn/syphotos/android/SyPhotosApplication.kt
@@ -7,6 +7,7 @@ import com.baidu.mapapi.SDKInitializer
 class SyPhotosApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        SDKInitializer.setAgreePrivacy(this, true)
         SDKInitializer.initialize(this)
         SDKInitializer.setCoordType(CoordType.BD09LL)
     }
